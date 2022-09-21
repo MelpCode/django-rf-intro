98aca8c89f5a1c12e299ac53ce1d2652c9947cf0
MelpCode <melp.code.01@gmail.com>
2022-09-20 19:33:50 -0500

    model Player added

--- players/models.py
+++ players/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+  name = models.CharField(max_length=200)
+  age = models.PositiveIntegerField()
+  height=models.FloatField()
+  subtitute=models.BooleanField()
+  country=models.CharField(max_length=200)
